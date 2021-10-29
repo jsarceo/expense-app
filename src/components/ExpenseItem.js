ef2262b010ec674842befa1229f19a39bfbe8c53
jsarceo <87043527+jsarceo@users.noreply.github.com>
2021-10-29 14:51:05 -0700

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28, 2021</div>
+
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$249.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
